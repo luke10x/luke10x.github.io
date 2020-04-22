70cd6c1f57108e0aefcad560e8b70900f2f60efe
Lukas Normantas <spam@discoverops.com>
2020-04-23 00:02:05 +0100

    Some content

--- www/src/components/header.js
+++ www/src/components/header.js
@@ -1,35 +1,77 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+
+
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
+
+const Header = ({ siteTitle }) => {
+  const classes = useStyles();
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          {/* <CameraIcon className={classes.icon} /> */}
+          <Typography variant="h6" color="inherit" noWrap>
+            Luke 10X Developer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -5,16 +5,32 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import "../components/layout.css"
+import 'typeface-roboto'
+import MainFeaturedPost from "../components/MainFeaturedPost"
+
+
+const mainFeaturedPost = {
+  title: 'There is nothing...',
+  description:
+    "...so far",
+  image: 'https://source.unsplash.com/random',
+  imgText: 'main image description',
+  linkText: 'TAXX - software to be on top of your tasks',
+  linkUrl: 'fsfds'
+};
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    <MainFeaturedPost post={mainFeaturedPost} />
+    <p>Under construction</p>
+
+    {/*
+    <Link to="/page-2/">Go to page 2</Link> | 
+    <Link to="/album/">Go to album</Link> | 
+    <Link to="/pricing/">Pricing</Link> | 
+    */}
   </Layout>
 )
 
